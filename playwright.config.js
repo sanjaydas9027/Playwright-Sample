08c7592187c7e80d1d70e34287a7bb7aca14b94b
sanjaydas9027 <sanjaydas9027@gmail.com>
2024-07-11 22:49:24 +0530

    added ReadMe.md file

--- playwright.config.js
+++ playwright.config.js
@@ -32,7 +32,7 @@ module.exports = defineConfig({
     // baseURL: 'http://127.0.0.1:3000',
     baseURL: "https://rahulshettyacademy.com",
     browserName: "chromium",
-    headless: false,
+    headless: true,
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: "on-first-retry",
   },
